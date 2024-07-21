67224cbc0cefeb8642d0d43b3fbb164cd73b7030
bayazidsustami <bayazidsustamy@gmail.com>
2024-07-21 21:23:39 +0700

    feat : creat primary button

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,26 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
 
-export default function Button ({label}){
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function Button ({label, theme}){
+    if(theme == "primary") {
+        return (
+            <View style={[styles.buttonContainer, { borderWidth:4, borderColor:"#ffd33d", borderRadius:18 }]}>
+                <Pressable
+                    style={[styles.button, { backgroundColor:"#fff" }]}
+                    onPress={() => alert('you pressed the button')}
+                >
+                    <FontAwesome
+                        name="picture-o"
+                        size={18}
+                        color={"#25292e"}
+                        style={styles.buttonIcon}
+                    />
+                    <Text style={[styles.buttonLabel, { color:"#25292e" }]}></Text>
+                </Pressable>
+            </View>
+        )
+    }
     return(
         <View style={styles.buttonContainer}>
             <Pressable style={styles.button} onPress={() => alert('you pressed the button')}>
